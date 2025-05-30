9dab83272c1f0b4a45e2a85671063afdc226e452
kazu777keisoku <k291086@kansai-u.ac.jp>
2025-05-30 13:31:16 +0900

    test commit

---
+++ src/gbdepth_pkg/include/gbdepth_pkg/camera_node.hpp
@@ -0,0 +1,27 @@
+#ifndef GBDEPTH_PKG__AMERA_NODE
+#define GBDEPTH_PKG__AMERA_NODE
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/image_encodings.hpp>
+#include <std_msgs/msg/header.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+using namespace std;
+
+class CameraNode : public rclcpp::Node
+{
+    public:
+        explicit CameraNode(const rclcpp::NodeOptions & options);
+    
+    private:
+        void callback();
+        
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        cv::VideoCapture cap;
+        cv::Mat frame;
+};
+
+#endif GBDEPTH_PKG__AMERA_NODE
